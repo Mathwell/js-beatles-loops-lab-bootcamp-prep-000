4c054b8fcfdfa12a1b5120d72ca81696a1f44f4e
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 14:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var array=[]
   for(var i=0; i<arrayMusicians.length; i++){
-    var string = arrayMusicians[i]+" plays "+arrayInstruments[i]+"."
+    var string = arrayMusicians[i]+" plays "+arrayInstruments[i]
     array.push(string)
   }
   return(array)
