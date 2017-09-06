47835587cfc43dfb8395059e2548ab30f50e2731
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 15:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ function johnLennonFacts(arrayFacts){
   }
   return(newArray)
 }
+
+function iLoveTheBeatles(n){
+  var array=[]
+  do{
+    array.push("I love the Beatles")
+    n++
+  }while n<15
+  return(array)
+}
