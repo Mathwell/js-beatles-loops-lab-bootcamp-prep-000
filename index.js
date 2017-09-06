e7276c493f44d3702adab57ae62bb66a3ac8e9db
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 15:13:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(arrayFacts){
 function iLoveTheBeatles(n){
   var array=[]
   do{
-    array.push("I love the Beatles")
+    array.push("I love the Beatles!")
     n++
   }while (n<15)
   return(array)
